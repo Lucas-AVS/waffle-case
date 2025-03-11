4d9a796d64db111ba96d1c25a785db4d972816b5
Lucas-AVS <lucas.alencar31@gmail.com>
2025-03-11 19:39:23 -0300

    refactor: move app logic from main to App component for better code organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { waffleColors } from "./styles/themes/waffle-colors"
-// import AdminDashboard from "./pages/AdminDashboard"
-// import SignUp from "./pages/SignUp/index"
-import LogIn from "./pages/LogIn"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import AuthProvider from './context/AuthProvider';
+import { waffleColors } from './styles/themes/waffle-colors';
+import { GlobalStyle } from './styles/global';
+import ProtectedRoute from './context/ProtectedRouteProps';
+import LogIn from './pages/LogIn';
+import AdminDashboard from './pages/AdminDashboard';
+import UserDashboard from './pages/UserDashboard';
+
+const NotFound = () => {
+  return <h1>404 - Not Found</h1>;
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LogIn />,
+  },
+  {
+    path: '/admin',
+    element: (
+      <ProtectedRoute>
+        <AdminDashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/user/:email',
+    element: (
+      <ProtectedRoute>
+        <UserDashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 function App() {
   return (
-    <ThemeProvider theme={waffleColors}>
-      {/* <SignUp></SignUp> */}
-      {/* <AdminDashboard></AdminDashboard> */}
-      <LogIn></LogIn>
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+    <AuthProvider>
+      <ThemeProvider theme={waffleColors}>
+        <RouterProvider router={router} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
-
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LogIn from './pages/LogIn';
-import AdminDashboard from './pages/AdminDashboard';
-import AuthProvider from './context/AuthProvider';
-import { ThemeProvider } from 'styled-components';
-import { waffleColors } from './styles/themes/waffle-colors';
-import { GlobalStyle } from './styles/global';
-import ProtectedRoute from './context/ProtectedRouteProps';
-import UserDashboard from './pages/UserDashboard';
-
-const NotFound = () => {
-  return <h1>404 - Not Found</h1>;
-};
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LogIn />,
-  },
-  {
-    path: '/admin',
-    element: (
-      <ProtectedRoute>
-        <AdminDashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '/user/:email',
-    element: (
-      <ProtectedRoute>
-        <UserDashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-]);
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <ThemeProvider theme={waffleColors}>
-        <RouterProvider router={router} />
-        <GlobalStyle />
-      </ThemeProvider>
-    </AuthProvider>
+    <App />
   </StrictMode>,
 );
